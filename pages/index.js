db5746645f334146d03f0a0c5eaf8100f2a66591
Arkan94 <xlarkanlx@gmail.com>
2019-09-19 21:30:27 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Sample app using React and Next.js</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
